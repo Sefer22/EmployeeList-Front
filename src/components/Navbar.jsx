4e09e3ebf33660e384440d4fd585dfee67303647
Sefer22 <sfrs87997@gmail.com>
2024-08-14 16:52:31 +0400

    developed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='bg-gray-800'>
+      <div>
+        <p>Employee Management System</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    contennt:["./src/**/*.{js,jsx,ts,tsx}"],
+    theme:{
+        extend:{},
+    },
+    plugins:[],
+};

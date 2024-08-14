9357379941867fda61fb5adefde6e557487dbfb1
Sefer22 <sfrs87997@gmail.com>
2024-08-14 17:34:38 +0400

    developed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import '../App.css'
 
 function Navbar() {
   return (
-    <div className='bg-gray-600'>
+    <div className='bg-gray-800'>
       <div className='h-16 px-8 flex items-center'>
-        <p>Employee Management System</p>
+        <p className='text-white font-bold'>Employee Management System</p>
       </div>
     </div>
   )

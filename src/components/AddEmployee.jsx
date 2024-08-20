68ee0881fb632531c211371021b68622d9135038
Sefer22 <sfrs87997@gmail.com>
2024-08-20 19:37:02 +0400

    em

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddEmployee = () => {
+
+    const [employee,setEmployee] = useState({
+        id:"",
+        firstName:"",
+        lastName:"",
+        email:""
+    })
+
+    const handleChange = (e)=>{
+        const value = e.target.value;
+        setEmployee({...employee,[e.target.name] : value})
+    }
+
     return (
         <div className='flex max-w-2xl mx-auto shadow border-b'>
             <div className='px-8 py-8'>
@@ -10,7 +23,7 @@ const AddEmployee = () => {
 
                 <div className='items-center justify-center h-14 w-full my-4'>
                     <label className='block text-gray-600 text-sm font-normal'>First Name</label>
-                    <input type="text" name="firstname"
+                    <input type="text" name="firstName"
                         className='h-10 w-96 border mt-2 px-2 py-2'
                         value={employee.firstName}
                         onChange={(e) => handleChange(e)} />
@@ -25,7 +38,7 @@ const AddEmployee = () => {
 
                 <div>
                     <label className='block text-gray-600 text-sm font-normal'>Email</label>
-                    <input type="text" name="email"
+                    <input type="email" name="email"
                         className='h-10 w-96 border mt-2 px-2 py-2'
                         value={employee.email}
                         onChange={(e) => handleChange(e)} />

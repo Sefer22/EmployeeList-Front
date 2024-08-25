63c388ebb70c09381a044c4648c9d4eeec64def0
Sefer22 <sfrs87997@gmail.com>
2024-08-26 02:19:58 +0400

    em

--- src/service/EmployeeService.jsx
+++ src/service/EmployeeService.jsx
@@ -1,1 +1,20 @@
+import axios from 'axios';
+
+const EMPLOYEE_BASE_API_URL = "http://localhost:8080/api/v1";
+
+class EmployeeService {
+    saveEmployee(employee) {
+        return axios.post(EMPLOYEE_BASE_API_URL+"/new",employee);
+    }
+    getAllEmployees() {
+        return axios.get(EMPLOYEE_BASE_API_URL+"/all");
+    }
+    deleteEmployee(id) {
+        return axios.delete(EMPLOYEE_BASE_API_URL+"/"+id);
+    }
+    getEmployeeById(id) {
+        return axios.get(EMPLOYEE_BASE_API_URL + "/"+id);
+    }
+}
+
 export default new EmployeeService();
